4ebf2b0a345bf4bda60d601b3e3f83f2e2c0c89e
socra167 <socra167@gmail.com>
2025-04-16 12:44:32 +0900

    chore: S3 테스트가 Credential 설정 없이 가능하도록 변경

--- src/main/kotlin/com/ec2ex/S3ServiceImpl.kt
+++ src/main/kotlin/com/ec2ex/S3ServiceImpl.kt
@@ -1,7 +1,9 @@
 package com.ec2ex
 
+import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.s3.S3Client
 
+@Service
 class S3ServiceImpl(
     private val s3Client: S3Client,
 ) : S3Service {
